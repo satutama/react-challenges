fdff64232ae8e3f9984e2f011b2aee380b3405d7
Satria Utama <satria.u.s@gmail.com>
2023-09-04 20:34:54 +0200

    Updates

---
+++ static/7TalscMkKGSC4OTQ85Qoh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/6e3ffec812d88e3d.css","static/chunks/pages/index-f7a9bc2cec4cca44.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
